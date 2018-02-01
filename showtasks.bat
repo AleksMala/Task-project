21dc941a1a3e7842896165f90997744867210e37
AleksMala <o.malaszuk@gmail.com>
2018-02-01 08:04:06 +0100

    First attempt to Heroku

--- showtasks.bat
+++ showtasks.bat
@@ -5,10 +5,8 @@ echo RUNCRUD has errors
 goto fail
 
 :getsite
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080/crud/v1/task/getTasks
-if "%ERRORLEVEL%" == "0" goto end
-echo Cannot open window
-goto fail
+start chrome "http://localhost:8080/crud/v1/task/getTasks"
+goto end
 
 :fail
 echo.
